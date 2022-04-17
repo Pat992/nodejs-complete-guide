a78cc479b291745f53ea18225b603ef0fde54ff1
Patrick Hettich <patrick_hettich@hotmail.com>
2022-04-17 19:08:37 +0200

    basic routing done

--- Basics/app.js
+++ Basics/app.js
@@ -5,15 +5,33 @@ const http = require('http');
 
 // Create server with request and response callback
 const server = http.createServer((req, res) => {
-    console.log(req);
-    // Set response
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Respons</title></head>');
-    res.write('<body><h1>Response</h1></body>');
-    res.write('</html>');
-    // End and send response
-    res.end();
+    const url = req.url;
+
+    if (url === '/') {
+        // Set response
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Input</title></head>');
+        res.write(`
+        <body>
+        <form action="/message" method="POST">
+        <input type="text">
+        <button type="submit">Send</button>
+        </form>
+        </body>`);
+        res.write('</html>');
+        // End and send response -> "return" is required to end function, else nothing else can be set in res
+        return res.end();
+    } else if (url === '/message' && req.method === 'POST') {
+        // Set response
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Message</title></head>');
+        res.write('<body><h1>Message sent</h1></body>');
+        res.write('</html>');
+        // End and send response
+        return res.end();
+    }
 });
 
 // Start server-process
