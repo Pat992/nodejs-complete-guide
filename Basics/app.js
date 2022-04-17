cf56c3f20d268351a93fd90897e406f22684ca02
Patrick Hettich <patrick_hettich@hotmail.com>
2022-04-17 19:00:56 +0200

    simple response done

--- Basics/app.js
+++ Basics/app.js
@@ -6,6 +6,14 @@ const http = require('http');
 // Create server with request and response callback
 const server = http.createServer((req, res) => {
     console.log(req);
+    // Set response
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Respons</title></head>');
+    res.write('<body><h1>Response</h1></body>');
+    res.write('</html>');
+    // End and send response
+    res.end();
 });
 
 // Start server-process
