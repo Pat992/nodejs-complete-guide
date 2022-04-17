22c940f269c7cfab569e44542b5b1fe235911f66
Patrick Hettich <patrick_hettich@hotmail.com>
2022-04-17 19:25:38 +0200

    saved body to file

--- Basics/app.js
+++ Basics/app.js
@@ -16,7 +16,7 @@ const server = http.createServer((req, res) => {
         res.write(`
         <body>
         <form action="/message" method="POST">
-        <input type="text">
+        <input type="text" name="message">
         <button type="submit">Send</button>
         </form>
         </body>`);
@@ -24,14 +24,26 @@ const server = http.createServer((req, res) => {
         // End and send response -> "return" is required to end function, else nothing else can be set in res
         return res.end();
     } else if (url === '/message' && req.method === 'POST') {
-        // Write new file
-        fs.writeFileSync('message.txt', 'TEST');
-        // redirect user back to /
-        res.writeHead(302, {
-            'Location': '/'
+        const body = [];
+        // register request data event listener -> each chunk will execute on every data event
+        req.on('data', (chunk) => {
+            // add chunks into body element
+            body.push(chunk);
         });
-        // End and send response
-        return res.end();
+        // add listener to execute on end
+        req.on('end', () => {
+            // buffer the chunks -> funny sentence
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            // Write new file
+            fs.writeFileSync('message.txt', message);
+            // redirect user back to /
+            res.writeHead(302, {
+                'Location': '/'
+            });
+            // End and send response (always add return, as mentioned above)
+            return res.end();
+        });
     }
 });
 
