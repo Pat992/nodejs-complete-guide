b24b10e38993d132256e33c0d2117c87bc9e2a43
Patrick Hettich <patrick_hettich@hotmail.com>
2022-04-17 21:44:16 +0200

    added // @ts-check for some ts-error-handling

--- Express/app.js
+++ Express/app.js
@@ -1,10 +1,15 @@
+// Pretty cool that comment
+// @ts-check
+
 const http = require('http');
 
 // const rqListener = (req, res) => { }
 // http.createServer(rqListener);
 
 // Create server with request and response callback
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+
+});
 
 // Start server-process
 server.listen(3000);
