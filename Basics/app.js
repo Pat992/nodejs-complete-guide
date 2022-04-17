716fbd87499fec3fb1696f8a09b665bc1a76dc2a
Patrick Hettich <patrick_hettich@hotmail.com>
2022-04-17 19:11:31 +0200

    writing message, redirecting user

--- Basics/app.js
+++ Basics/app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 // const rqListener = (req, res) => { }
 // http.createServer(rqListener);
@@ -23,12 +24,12 @@ const server = http.createServer((req, res) => {
         // End and send response -> "return" is required to end function, else nothing else can be set in res
         return res.end();
     } else if (url === '/message' && req.method === 'POST') {
-        // Set response
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Message</title></head>');
-        res.write('<body><h1>Message sent</h1></body>');
-        res.write('</html>');
+        // Write new file
+        fs.writeFileSync('message.txt', 'TEST');
+        // redirect user back to /
+        res.writeHead(302, {
+            'Location': '/'
+        });
         // End and send response
         return res.end();
     }
