0a7fa99aed58b612de27b4ef36aa8d22d3a93fdc
Patrick Hettich <patrick_hettich@hotmail.com>
2022-04-17 19:35:31 +0200

    made filesave async

--- Basics/app.js
+++ Basics/app.js
@@ -15,10 +15,10 @@ const server = http.createServer((req, res) => {
         res.write('<head><title>Input</title></head>');
         res.write(`
         <body>
-        <form action="/message" method="POST">
-        <input type="text" name="message">
-        <button type="submit">Send</button>
-        </form>
+            <form action="/message" method="POST">
+                <input type="text" name="message">
+                <button type="submit">Send</button>
+            </form>
         </body>`);
         res.write('</html>');
         // End and send response -> "return" is required to end function, else nothing else can be set in res
@@ -36,13 +36,14 @@ const server = http.createServer((req, res) => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
             // Write new file
-            fs.writeFileSync('message.txt', message);
-            // redirect user back to /
-            res.writeHead(302, {
-                'Location': '/'
+            fs.writeFile('message.txt', message, (err) => {
+                // redirect user back to /
+                res.writeHead(302, {
+                    'Location': '/'
+                });
+                // End and send response (always add return, as mentioned above)
+                return res.end();
             });
-            // End and send response (always add return, as mentioned above)
-            return res.end();
         });
     }
 });
