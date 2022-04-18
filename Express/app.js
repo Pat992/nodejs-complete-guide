648a433895bd7112196611c3615a86eec219a2fc
Patrick Hettich <patrick_hettich@hotmail.com>
2022-04-18 11:47:36 +0200

    added 404 route

--- Express/app.js
+++ Express/app.js
@@ -13,6 +13,13 @@ app.use(express.urlencoded());
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+// 404 router
+app.use((req, res, next) => {
+    res
+        .status(404)
+        .send('<h1>404 Page not found</h1>');
+});
+
 app.listen(port, () => {
     console.log(`Server started on port port`);
 });
