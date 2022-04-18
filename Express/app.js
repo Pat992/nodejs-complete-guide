9dfc158a2e64c659303f77e3519bd78aeced7553
Patrick Hettich <patrick_hettich@hotmail.com>
2022-04-18 11:42:06 +0200

    added routing

--- Express/app.js
+++ Express/app.js
@@ -1,14 +1,17 @@
 // @ts-check
 const express = require('express');
+// Get routes
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 const port = process.env.PORT || 3000;
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-app.use(express.json());
-app.get('/', (req, res) => {
-    res.end('Working');
-});
+app.use(express.urlencoded());
+// use Routes
+app.use(shopRoutes);
+app.use(adminRoutes);
 
 app.listen(port, () => {
     console.log(`Server started on port port`);
