c6c69e209d212e5bb1be38e2e6b18338533dcb2e
Patrick Hettich <patrick_hettich@hotmail.com>
2022-04-18 12:06:16 +0200

    added 404 file

--- Express/app.js
+++ Express/app.js
@@ -1,5 +1,6 @@
 // @ts-check
 const express = require('express');
+const path = require('path');
 // Get routes
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -17,7 +18,7 @@ app.use(shopRoutes);
 app.use((req, res, next) => {
     res
         .status(404)
-        .send('<h1>404 Page not found</h1>');
+        .sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(port, () => {
