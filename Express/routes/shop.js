e5c8974f94ff68fd1206571ce41c49f1e1f70138
Patrick Hettich <patrick_hettich@hotmail.com>
2022-04-18 12:11:02 +0200

    rootDir helper added

--- Express/routes/shop.js
+++ Express/routes/shop.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
     // use path.jon and __dirname to build path (don't use slashes, linux/windows)
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
