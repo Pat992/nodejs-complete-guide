1926a96462cc71ba117d89f3fd7f806a8bafd4f7
Patrick Hettich <patrick_hettich@hotmail.com>
2022-04-18 12:03:43 +0200

    updated routes to return html

--- Express/routes/shop.js
+++ Express/routes/shop.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Homepage');
+    // use path.jon and __dirname to build path (don't use slashes, linux/windows)
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
