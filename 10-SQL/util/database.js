a83344bfaa327b5aab9a68d5b412b5a47012e0f4
Patrick Hettich <patrick_hettich@hotmail.com>
2022-05-07 12:23:53 +0200

    created sequelize product-model

--- 10-SQL/util/database.js
+++ 10-SQL/util/database.js
@@ -1,10 +1,19 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'product-db',
-    password: ''
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'product-db',
+//     password: ''
+// });
+
+// module.exports = pool.promise();
+// @ts-check
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('product_db', 'root', '', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
